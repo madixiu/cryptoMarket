af3f259735c0e196a1733658a19ffb6f13f2267d
Madix <mahdi.moradi72@gmail.com>
2022-03-30 13:42:50 +0430

    added crypto icons

--- components/ListDetail.js
+++ components/ListDetail.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text,Image } from 'react-native'
+import jsonData from '../assets/data/coinMarketCapIDlist.json'
 
 const textColor = (input) => {
   if (input > 0)
@@ -9,11 +10,20 @@ const textColor = (input) => {
   else
   return {color: 'black'}
 }
+const iconIDfinder = (symbol) => {
+  let IDdata = jsonData
+  return IDdata[symbol].id
+}
 const ListDetail = props => {
- 
+  
   return (
     <View style={styles.itemView}>
-      <Text style={styles.symbolTextStyling}>{props.symbol}</Text>
+      <View style={styles.IconViewStyle}>
+        <Image source={{ uri: 'https://s2.coinmarketcap.com/static/img/coins/64x64/'+iconIDfinder(props.symbol)+'.png', cache: 'force-cache'}} 
+          style={{ width: 32, height: 32,marginLeft: 10, }}
+        />
+        <Text style={styles.symbolTextStyling}>{props.symbol}</Text>
+      </View>
       <Text style={styles.priceTextStyling}>{props.price}</Text>
       <Text style={[styles.changeTextStyling, textColor(props.change)]} >{props.change}%</Text>
 
@@ -25,8 +35,8 @@ const ListDetail = props => {
 const styles = {
   itemView: {
     backgroundColor: 'gainsboro',
-    marginLeft: 4,
-    marginRight:4,
+    marginLeft: 5,
+    marginRight:5,
     marginBottom: 5,
     borderRadius: 7,
     // flex:1,
@@ -34,13 +44,10 @@ const styles = {
     alignItems: 'center',
     flexDirection: 'row',
     minHeight: 74
-
-
-    
   },
   symbolTextStyling: {
     fontSize: 15,
-    paddingLeft: 22,
+    paddingLeft: 10,
   },
   priceTextStyling: {
     fontSize: 15,
@@ -49,13 +56,18 @@ const styles = {
     fontSize: 15,
     paddingRight: 22,
   },
-  viewStyle: {
-    backgroundColor: 'gainsboro',
-    height: 70,
+  IconViewStyle: {
+    // backgroundColor: 'gainsboro',
+    // height: 70,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: 25
+    // paddingTop: 25
 
-  }
+  },
+  logo: {
+    width: 16,
+    height: 16,
+  },
 }
 export default ListDetail
